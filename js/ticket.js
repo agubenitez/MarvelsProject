b8636ec72ea94150033bdf79a6dbd440ff2f4ace
agubenitez <69943978+agubenitez@users.noreply.github.com>
2022-02-16 04:52:56 -0300

    Boton eliminar

--- js/ticket.js
+++ js/ticket.js
@@ -19,57 +19,52 @@ var currentTicket = [];
 var ultimoTicket = []
 var cant = 1;
 var regNum = 0;
-
-var menu = [
-
-]
-
 var burguers = [
     {
         "name": "AntMan",
         "id": "a",
         "description": "",
-        "cost": 245,
-        "costInterface": 245,
+        "cost": 270,
+        "costInterface": 270,
         "currency": "$"
     },
     {
         "name": "Hukl",
         "id": "h",
         "description": "",
-        "cost": 355,
-        "costInterface": 355,
+        "cost": 380,
+        "costInterface": 380,
         "currency": "$"
     },
     {
         "name": "Capitan America",
         "id": "c",
         "description": "",
-        "cost": 315,
-        "costInterface": 315,
+        "cost": 365,
+        "costInterface": 365,
         "currency": "$"
     },
     {
         "name": "Iron Man",
         "id": "i",
         "description": "",
-        "cost": 375,
-        "costInterface": 375,
+        "cost": 395,
+        "costInterface": 395,
         "currency": "$"
     },
     {
         "name": "4 Fantastic",
         "id": "4",
         "description": "",
-        "cost": 365,
-        "costInterface": 365,
+        "cost": 395,
+        "costInterface": 363955,
         "currency": "$"
     },
     {
         "name": "Marvel",
         "id": "m",
         "description": "",
-        "cost": 525,
+        "cost": 595,
         "costInterface": 525,
         "currency": "$"
     },
@@ -90,7 +85,7 @@ var burguers = [
         "currency": "$"
     },
     {
-        "name": "PorcionBravas",
+        "name": "Bravas",
         "id": "pbr",
         "description": "",
         "cost": 300,
@@ -98,7 +93,7 @@ var burguers = [
         "currency": "$"
     },
     {
-        "name": "PorcionAros",
+        "name": "Aros",
         "id": "par",
         "description": "",
         "cost": 300,
@@ -106,15 +101,15 @@ var burguers = [
         "currency": "$"
     },
     {
-        "name": "PorcionPapas",
+        "name": "Papas Fritas",
         "id": "pp",
         "description": "",
-        "cost": 225,
-        "costInterface": 225,
+        "cost": 245,
+        "costInterface": 245,
         "currency": "$"
     },
     {
-        "name": "PorcionPapasChedar",
+        "name": "PapasChedar",
         "id": "ppc",
         "description": "",
         "cost": 0,
@@ -122,8 +117,8 @@ var burguers = [
         "currency": "$"
     },
     {
-        "name": "PorcionPapasChedarBacon",
-        "id": "ppc",
+        "name": "PapasChedarBacon",
+        "id": "ppcb",
         "description": "",
         "cost": 0,
         "costInterface": 0,
@@ -138,11 +133,11 @@ var burguers = [
         "currency": "$"
     },
     {
-        "name": "15% de descuento!!!",
+        "name": "15% de descuento",
         "id": "d",
         "description": "",
         "cost": 0,
-        "costInterface": "- 15% !!! ",
+        "costInterface": "15%",
         "currency": "$"
     },
 
@@ -153,68 +148,8 @@ function imprimir() {
     window.print();
 }
 
-function showLastTicket(ultimoTicket) {
 
-    // parrafoNombre.appendChild(ultimoTicket[ultimoTicket.length-1].nombre)
-    // parrafoTelefono.appendChild(ultimoTicket[ultimoTicket.length-1].telefono)
-    let htmlContentToAppend = ""
-    //voy hasta el length -1 porque en la ultima posicion esta el cliente
-    //los datos del cliente estan en: ultimoTicket.[length-1]
-
-    for (let i = 0; i < ultimoTicket.length - 1; i++) {
-        let ticket = ultimoTicket[i];
-
-        htmlContentToAppend +=
-            `
-            <tr>
-            <td class="cantidad">`+ cant + `</td>
-            <td class="producto">`+ ticket.name + `   <br> ` + ticket.notas + `  </td>
-
-            <td class="precio">`+ ticket.currency + ticket.cost + `</td>
-            </tr>
-
-        `
-    }
-
-    tbody.innerHTML = htmlContentToAppend;
-    total = sumarTotalUltimo(ultimoTicket)
-
-    trTotal.innerHTML = `
-        <td></td>
-        <td>TOTAL</td>
-        <td id="total">$`+ total + `</td>`
-
-//cliente
-let cliente = ultimoTicket[ultimoTicket.length-1]
-
-        document.getElementById("divCliente").innerHTML = `
-
-        <p id="parrafoDir" class="centrado">`+ cliente.currentCliente.direccion.data+`
-        </p>
-       <p id="parrafoNombre" class="centrado">
-       `+ cliente.currentCliente.nombre.data+`
-        </p>
-       <p id="parrafoTelefono" class="centrado">
-       `+ cliente.currentCliente.telefono.data+`    </p>`
-
-}
-
-function sumarTotalUltimo(array) {
-    var total = 0
-    for (let i = 0; i < array.length - 1; i++) {
-        var a = array[i]
-        var costo = parseInt(a.cost)
-        total = total + costo
-        if (a.id == "descuento") {
-            total = Math.round(total * 0.85)
-        }
-    }
-    return (total);
-
-}
-
-
-function showTicket(array) {
+function showTicket() {
 
     let htmlContentToAppend = "";
     for (let i = 0; i < currentTicket.length; i++) {
@@ -225,9 +160,14 @@ function showTicket(array) {
     <td class="cantidad">`+ cant + `</td>
     <td class="producto">`+ ticket.burguer.name + `   <br> ` + ticket.notas + `  </td>
 
-    <td class="precio">`+ ticket.burguer.currency + ticket.burguer.cost + `</td>
+    <td class="precio">`+ ticket.burguer.currency + ticket.burguer.costInterface + `</td>
+     
+    <td><button class="oculto-impresion" onclick="eliminarItem(`+ i + `)" >x</button></td>
     </tr>
-    <button onclick="eliminarItem(`+ i + `)" class="ml-2 badge badge-danger">Eliminar</button>
+
+    
+    
+    
 
 `
     }
@@ -245,17 +185,22 @@ function showTicket(array) {
 `
 
 
+
 }
 function eliminarItem(i) {
     //pregunto al usuario si esta seguro
     var option = confirm("¿Esta seguro que deseaz eliminar el articulo?");
     //si option es true elimino el articulo, sino no hace nada
-   if(option){
-   
-    currentTicket[i]
-   }
-  }
-  
+    if (option) {
+
+        currentTicket.splice(i, 1)
+
+        console.log(currentTicket[i])
+
+        showTicket()
+    }
+}
+
 //fin show ticket
 
 
@@ -275,31 +220,17 @@ function agregarProd(id, notas) {
 
             currentTicket.push({ notas, burguer })
             console.log(currentTicket)
+    
+            showTicket()
+        
 
-            showTicket(currentTicket)
 
-//             htmlContentToAppend +=
-//                 `
-//     <tr>
-//     <td class="cantidad">`+ cant + `</td>
-//     <td class="producto">`+ burguer.name + `   <br> ` + notas + `  </td>
-
-//     <td class="precio">`+ burguer.currency + burguer.cost + `</td>
-//     </tr>
-
-// `
 
         }
 
 
     }
-//     tbody.innerHTML += htmlContentToAppend;
-//    let total = sumarTotal(currentTicket)
 
-//     trTotal.innerHTML = `
-// <td></td>
-// <td>TOTAL</td>
-// <td id="total">$`+ total + `</td>`
 
 }
 //fin agregar prod
@@ -312,8 +243,9 @@ function sumarTotal(array) {
         total = total + costo
 
         if (a.burguer.id == "d") {
-            
+
             total = Math.round(total * 0.85)
+                  
         }
     }
     return (total);
@@ -323,7 +255,8 @@ function sumarTotal(array) {
 
 function saveTicket(ticket) {
     ultimoTicket = []
-    ultimoTicket.push ({ticket});
+   
+    ultimoTicket.push({ ticket });
     ultimoTicket.push({ currentCliente })
     regNum++
     localStorage.setItem(regNum, JSON.stringify(ultimoTicket));
@@ -331,25 +264,23 @@ function saveTicket(ticket) {
 
 function cargarSelect() {
     var prodSelect = document.getElementById("idProd");
-              
-        for (let i = 0; i < burguers.length; i++) {
-            let burguer = burguers[i];
 
-            var option=document.createElement("option");
-            option.value= burguer.id
-            option.text= burguer.name
-            prodSelect.appendChild(option) 
-    
-        }
+    for (let i = 0; i < burguers.length; i++) {
+        let burguer = burguers[i];
+
+        var option = document.createElement("option");
+        option.value = burguer.id
+        option.text = burguer.name
+        prodSelect.appendChild(option)
+
+    }
 }
-
-
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
-   cargarSelect()
-        
-  
+    cargarSelect()
+
+
     document.getElementById("agregar").addEventListener("click", function () {
         idProd = document.getElementById("idProd").value
         inputNotas = document.getElementById("inputNotas").value
@@ -375,34 +306,38 @@ document.addEventListener("DOMContentLoaded", function (e) {
     });
 
     document.getElementById("botonDir").addEventListener("click", function () {
-    //    document.getElementById("parrafoDir").innerHTML = "";
-        var direccion = document.createTextNode(document.getElementById("inputDir").value)
-        parrafoDir.appendChild(direccion)
+        var direccion = document.getElementById("inputDir").value
+
+        parrafoDir.innerHTML = direccion
         currentCliente.direccion = direccion;
 
 
     });
     document.getElementById("botonNombre").addEventListener("click", function () {
-        // document.getElementById("parrafoNombre").innerHTML = '';
 
-        var contenido = document.createTextNode(document.getElementById("inputNombre").value)
-        parrafoNombre.appendChild(contenido)
-        console.log(contenido)
-        currentCliente.nombre = contenido;
+        // var contenido = document.createTextNode(document.getElementById("inputNombre").value)
+        // parrafoNombre.appendChild(contenido)
+        // console.log(contenido)
+        // currentCliente.nombre = contenido;
+
+        var nombre = document.getElementById("inputNombre").value
+        parrafoNombre.innerHTML = nombre
+        currentCliente.nombre = nombre;
 
     });
     document.getElementById("botonTelefono").addEventListener("click", function () {
         // document.getElementById("parrafoTelefono").innerHTML = '';
 
-        var telefono = document.createTextNode(document.getElementById("inputTelefono").value)
-        parrafoTelefono.appendChild(telefono)
+        // var telefono = document.createTextNode(document.getElementById("inputTelefono").value)
+        // parrafoTelefono.appendChild(telefono)
+        // currentCliente.telefono = telefono;
+
+        var telefono = document.getElementById("inputTelefono").value
+        parrafoTelefono.innerHTML = telefono
         currentCliente.telefono = telefono;
 
     });
-    document.getElementById("botonUltimoTicket").addEventListener("click", function () {
-     //  showLastTicket(ultimoTicket);
-   
-    });
+
 
 
 });
@@ -410,3 +345,64 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
 
 /// ultimoTicket[ultimoTicket.length-1].currentCliente.nombre
+
+// function showLastTicket(ultimoTicket) {
+
+//     // parrafoNombre.appendChild(ultimoTicket[ultimoTicket.length-1].nombre)
+//     // parrafoTelefono.appendChild(ultimoTicket[ultimoTicket.length-1].telefono)
+//     let htmlContentToAppend = ""
+//     //voy hasta el length -1 porque en la ultima posicion esta el cliente
+//     //los datos del cliente estan en: ultimoTicket.[length-1]
+
+//     for (let i = 0; i < ultimoTicket.length - 1; i++) {
+//         let ticket = ultimoTicket[i];
+
+//         htmlContentToAppend +=
+//             `
+//             <tr>
+//             <td class="cantidad">`+ cant + `</td>
+//             <td class="producto">`+ ticket.name + `   <br> ` + ticket.notas + `  </td>
+
+//             <td class="precio">`+ ticket.currency + ticket.cost + `</td>
+//             </tr>
+
+//         `
+//     }
+
+//     tbody.innerHTML = htmlContentToAppend;
+//     total = sumarTotalUltimo(ultimoTicket)
+
+//     trTotal.innerHTML = `
+//         <td></td>
+//         <td>TOTAL</td>
+//         <td id="total">$`+ total + `</td>`
+
+//     //cliente
+//     let cliente = ultimoTicket[ultimoTicket.length - 1]
+
+//     document.getElementById("divCliente").innerHTML = `
+
+//         <p id="parrafoDir" class="centrado">`+ cliente.currentCliente.direccion.data + `
+//         </p>
+//        <p id="parrafoNombre" class="centrado">
+//        `+ cliente.currentCliente.nombre.data + `
+//         </p>
+//        <p id="parrafoTelefono" class="centrado">
+//        `+ cliente.currentCliente.telefono.data + `    </p>`
+
+// }
+
+
+// function sumarTotalUltimo(array) {
+//     var total = 0
+//     for (let i = 0; i < array.length - 1; i++) {
+//         var a = array[i]
+//         var costo = parseInt(a.cost)
+//         total = total + costo
+//         if (a.id == "descuento") {
+//             total = Math.round(total * 0.85)
+//         }
+//     }
+//     return (total);
+
+// }
